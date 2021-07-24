47d03e6ec95f48987ce28e8433ab3f7988d88fc8
Yoshinori Kosaka <yoshinori.kosaka@welyco.com>
2021-07-24 19:23:30 +0900

    feature: eval boolean

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -23,6 +23,21 @@ func TestEvalIntegerExpression(t *testing.T) {
 	}
 }
 
+func TestEvalBooleanExpression(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected bool
+	}{
+		{input: "true", expected: true},
+		{input: "false", expected: false},
+	}
+
+	for _, test := range tests {
+		evaluated := testEval(test.input)
+		testBooleanObject(t, evaluated, test.expected)
+	}
+}
+
 func testEval(input string) object.Object {
 	l := lexer.New(input)
 	p := parser.New(l)
@@ -37,5 +52,15 @@ func testIntegerObject(t *testing.T, evaluated object.Object, expected int64) {
 
 	if evaluated.(*object.Integer).Value != expected {
 		t.Errorf("Expected %d, but got %d", expected, evaluated.(*object.Integer).Value)
+	}
+}
+
+func testBooleanObject(t *testing.T, evaluated object.Object, expected bool) {
+	if evaluated.Type() != object.BOOLEAN_OBJECT {
+		t.Errorf("Expected a boolean, but got %s", evaluated.Type())
+	}
+
+	if evaluated.(*object.Boolean).Value != expected {
+		t.Errorf("Expected %t, but got %t", expected, evaluated.(*object.Boolean).Value)
 	}
 }
